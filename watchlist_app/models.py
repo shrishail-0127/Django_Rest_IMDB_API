672dcc224bee0e91d45ff68310568827585367f5
shrishail-0127 <shriiipj420@gmail.com>
2024-08-29 13:34:42 +0530

    initial commit

---
+++ watchlist_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Movie(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    active = models.BooleanField()
+    
+    
+    def __str__(self):
+        return self.name
+    
+    
